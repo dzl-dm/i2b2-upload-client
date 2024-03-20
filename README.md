01a60885ce7c1ff188f7cde7b4f0229c1cd6b377
Marc Griffiths <marc.griffiths@innere.med.uni-giessen.de>
2024-03-20 11:16:10 +0100

    Docker image with interactive menu for processing

--- README.md
+++ README.md
@@ -7,27 +7,51 @@ We usually call this the `datasource.xml`. Its a custom xml definition of how th
 ## Running the application
 The initial aim is to allow the upload process to be run with low effort. This will evolve into a client with few dependencies and high ease of use - after enough development.
 
+### Docker mode
+Ensure you have docker installed (eg [docker desktop](https://docs.docker.com/desktop/install/windows-install/)) - [wsl2](https://learn.microsoft.com/en-us/windows/wsl/install) is an advisable pre-requisite to docker on windows for performance.
+Then start the DWH image as a local container with:
+```sh
+docker run --name local-dwh-client --rm -it -v my/local/datasources:/datasources -e log_verbosity=1 -e secret_key="ChangeMe" -e dwh_api_key="ChangeMe" ghcr.io/dzl-dm/i2b2-upload-client/i2b2-dwh-client
+```
+It will run interactively by offering you options for how to proceed.
+
+The `-v my/local/datasources:/datasources` maps the left side on your local filesystem to the right side within docker and will need to contain your datasources in this structure:
+ds1/datasource.xml
+ds2/datasource.xml
+dsN/datasource.xml
+> NOTE: Under windows, the left side must be be an absolute path, eg C:\Users\me\datasources
+
+The `datasource.xml` references csv files with data for patients, visits and observations. These references will be respected as previously, it may be necessary to adjust path separators from backslash to forward slash.
+
+The processing only adds or overwrites data in a sub-directory for each datasource called `client-output`, no other data is changed.
+
 ## How it works
-There are 3 stages to the process.
+There are 3 stages to the process which are handled by this client.
 1. Converting to fhir
 1. pseudonymising
 1. uploading
 
-This doesn't map exactly to ETL as extraction is a pre-requisite, the csv patient data is already extracted. So these cover most of the transformation part, however further trasnformation is required on the server side before loading into the i2b2 database.
+This doesn't map exactly to ETL (Extract, Transform, Load) as extraction is a pre-requisite; the csv patient data is already extracted. So these cover most of the transformation part, however further trasnformation is required on the server side before loading (server side) into the i2b2 database.
 
 ### Convert to fhir
-Once you have clean data in csv files and a matching helper-file, the following command will produce the data in fhir-xml:
+Once you have clean data in csv files and a matching helper-file (datasource.xml), the following command will produce the data in fhir-xml:
 ```sh
-i2b2-client-fhir -d datasource.xml > fhir-data.xml
+i2b2-client-fhir datasource.xml > fhir-data-raw.xml
 ```
-At this stage, the data is unchanged, but re-formatted. Identifiable data will remain until the next stage.
+At this stage, the data content is unchanged, but re-formatted. Identifiable data will remain until the next stage.
+
+> NOTE: This stage requires `java 11` - more recent versions of java have removed a feature which this code relies on
 
 ### Pseudonymise
-Here we create a pseudonym for each patient and remove the name information from the data. Comments are also removed from the fhir data as it could hold sensitive information. Each client will generate different pseudonyms for the same patient due to a locally generated secret key. If you are migrating your environment and want to keep patient pseudonym compatibility, keep a backup of your secret key - more details on that topic below:
+Here we create a pseudonym for each patient and remove the name information from the data. Comments are also removed from the fhir data as they could hold sensitive information. Each client will generate different pseudonyms for the same patient due to a locally generated secret key. You should aim to keep a backup of the secret key you're using (perhaps in a password manager) to ensure you can always use the same key:
 ```sh
 ./process-pid.py
 ```
 The output is still a fhir bundle in XML format, simply with the changes outlined above.
 
+> NOTE: This stage requires `python 3`
+
 ### Upload
-The DWH should present an API which the client can interface with. A simple way to interact with the API is to use cURL, but most programming languages offer a network library which could be used to communicate.
+The DWH presents an API which the client can interface with. A simple way to interact with the API is to use cURL, but most programming languages offer a network library which could be used to communicate.
+
+> NOTE: This stage requires the cURL command to be available, which is included in `git for windows`
---
+++ docker/coordinate_data_processing.sh
@@ -0,0 +1,209 @@
+#!/usr/bin/env bash
+
+## coordinate the processing:
+    # * SHELL: Scan data volume for new datasources to process (output to: fhir-bundle-{raw,dwh}.xml)
+	# * JAVA: csv + xml config -> xml fhir bundle
+	# * PYTHON: xml fhir bundle -> pseudonymised xml fhir bundle
+## Offer user what to do with the data or only query the DWH
+	# * cURL: xml fhir bundle -> DWH server
+
+df="%Y-%m-%d %H:%M:%S"
+function log_debug { [[ "${log_verbosity}" -ge 3 ]] && >&2 echo "[$(date +"$df")] DEBUG: ${@}"; }
+function log_info { [[ "${log_verbosity}" -ge 2 ]] && >&2 echo "[$(date +"$df")] INFO: ${@}"; }
+function log_warn { [[ "${log_verbosity}" -ge 1 ]] && >&2 echo "[$(date +"$df")] WARN: ${@}"; }
+function log_error { >&2 echo "[$(date +"$df")] ERROR: ${@}"; }
+>&2 echo "(stderr) logging verbosity set to: ${log_verbosity}"
+curl_args="-s"
+if [[ "${log_verbosity}" -ge 3 ]] ; then
+    curl_args="-v"
+fi
+case $log_verbosity in
+    0) export log_level=ERROR ;;
+    1) export log_level=WARNING ;;
+    2) export log_level=INFO ;;
+    3) export log_level=DEBUG ;;
+esac
+
+## Scan /datasources/ which is a mounted directory of the users local datasources
+## For each source, check if there are output files and compare the dates to the config and source files
+function scan_local_sources {
+    ## Check which local sources are available and when they were updated locally and remotely
+    # for dir in /datasources/*/; do
+    for dir in $(ls -1 /datasources/); do
+        if [ ! -d ${dir}/client-output ] ; then
+            echo "Source '${dir}' has not yet been processed"
+        else
+            echo "Source '${dir}' is also available"
+            ## TODO: Check dates...
+            ## Check modify dates of source files (datasource.xml and any <url> references inside it)
+            # source_date=
+            # output_date
+            # echo "Source '${dir}' was last updated on '' and last processed on ''"
+        fi
+    done
+}
+
+
+function initial_options {
+    ## Ask user if they want to process only 1 specific source, auto process all or skip to the API interaction
+    base_options=("Scan and process available local sources" "Process specific source" "Skip to API interaction options")
+    select opt in "${base_options[@]}" exit; do
+    ## Use option indicies so code looks neater
+    case $REPLY in
+            1) echo "Sorry, this function is not yet implemented!" ;;
+            2) process_local_source ;;
+            3) api_options ;;
+            $((${#base_options[@]}+1))) echo "exiting"
+                break 2;;
+            *) echo Unrecognised choice: $REPLY ;;
+    esac
+    done
+}
+
+function process_local_source {
+    ## Prepare the source data into a pseudonymised fhir-bundle
+    log_debug "Processing a specific source..."
+    read -p 'Please provide a source name (the directory name): ' sn
+    if [[ "$(cd /datasources && echo */ )" =~ "${sn}/" && ${sn} != "" ]] ; then
+        echo "Source exists ($sn)! Processing..."
+        cd /datasources/${sn}
+        mkdir -p client-output
+        rm client-output/*
+        /upload-client/fhir-transform/process_data.sh datasource.xml > client-output/fhir-bundle-raw.xml
+        export input_fn=client-output/fhir-bundle-raw.xml
+        export output_fn=client-output/fhir-bundle-dwh.xml
+        /upload-client/pseudonym/src/process-pid.py
+        return 0
+    else
+        echo "Dir not found: $sn"
+        return 1
+    fi
+}
+
+## Once all sources are processed (or skipped), present API interaction options (check sources, upload new, etc)
+function api_options {
+    ## Ask user what they want from the API, then call the relevant function (or command, if simple enough) to perform the action
+    api_options=("List sources known to DWH" "Query specific DWH source" "Push new data to DWH" "Process uploaded data" "Delete a specific DWH dataset")
+    select opt in "${api_options[@]}" exit; do
+        ## Use option indicies so code looks neater
+        case $REPLY in
+                1) curl ${curl_args} -X GET -H "x-api-key: ${dwh_api_key}" ${DWH_API_ENDPOINT%/}/datasource | jq ;;
+                2) query_dwh_source ;;
+                3) upload_source ;;
+                4) process_dwh_source ;;
+                5) delete_dwh_source ;;
+                $((${#api_options[@]}+1))) echo "exiting"
+                    break 2;;
+                *) echo Unrecognised choice: $REPLY ;;
+        esac
+    done
+}
+
+function ask_source_name {
+    ## Ask user for remote DWH source name (can't verify as it could be for uploading a new source)
+    question_text=${1:-"Please provide a source name"}
+    default_answer=$2
+    while true; do
+        read -p "${question_text}: " sn
+        ## Do some sort of sanity checks on name
+        # if [[ "$(cd /datasources && echo */ )" =~ "${sn}/" && ${sn} != "" ]] ; then
+        if [[ $sn =~ ^[A-Za-z][A-Za-z0-9_]* ]] ; then
+            log_debug "Source name ($sn) looks safe..."
+            echo "${sn}"
+            return 0
+        else
+            echo "Sorry, this name isn't valid: '${sn}'"
+            break
+        fi
+    done
+}
+
+function query_dwh_source {
+    ## Default show status for source, then prompt for info or error
+    source_name=$(ask_source_name)
+    curl ${curl_args} -X GET -H "x-api-key: ${dwh_api_key}" ${DWH_API_ENDPOINT%/}/datasource/${source_name}/etl | jq
+    api_get_source_options=("Get info" "Get error")
+    select opt in "${api_get_source_options[@]}" exit; do 
+        ## Use option indicies so code looks neater
+        case $REPLY in
+                1) curl ${curl_args} -X GET -H "x-api-key: ${dwh_api_key}" ${DWH_API_ENDPOINT%/}/datasource/${source_name}/etl/info ;;
+                2) curl ${curl_args} -X GET -H "x-api-key: ${dwh_api_key}" ${DWH_API_ENDPOINT%/}/datasource/${source_name}/etl/error ;;
+                $((${#api_get_source_options[@]}+1))) echo "exiting"
+                    break 2;;
+                *) echo Unrecognised choice: $REPLY ;;
+        esac
+    done
+}
+
+function upload_source {
+    ## PUT a local, pseudonymised fhir bundle to the server
+    local_source_name=$(ask_source_name "Please provide which local data source you want to upload (the directory name)")
+    remote_source_name=$(ask_source_name "Please provide the datasource name you wish to use in the DWH (could be same as local, or different)" "${local_source_name}")
+    if [ -d /datasources/${local_source_name} ] ; then
+        dwh_bundle_path="/datasources/${local_source_name}/client-output/fhir-bundle-dwh.xml"
+        if [ -d /datasources/${local_source_name}/client-output ] && [ -f "${dwh_bundle_path}" ] ; then
+            if curl ${curl_args} -X PUT -H "x-api-key: ${dwh_api_key}" ${DWH_API_ENDPOINT%/}/datasource/${remote_source_name}/fhir-bundle -F "fhir_bundle=@${dwh_bundle_path}" ; then
+                msg_core="The DWH has accepted new data for '${remote_source_name}' (from '${local_source_name}')."
+                msg_full="${msg_core} Please check the status until it changes from 'Uploading' -> 'Uploaded', then you can trigger the processing."
+                log_info ${msg_core} 
+                echo "${msg_full}"
+                return 0
+            else
+                fail_msg_core="Something went wrong uploading the data to the DWH for '${remote_source_name}' (from '${local_source_name}')."
+                fail_msg="${fail_msg_core} Please check your connectivity and contact the DWH maintainer if the problem persists."
+                log_warn ${fail_msg_core} 
+                echo "${fail_msg}"
+                return 1
+            fi
+        else
+            log_warn "Local source not fully processed '${local_source_name}'"
+            echo "Local source not fully processed '${local_source_name}'"
+            return 1
+        fi
+    else
+        log_warn "Local source not available '${local_source_name}'"
+        echo "Local source not available '${local_source_name}'"
+        return 1
+    fi
+}
+
+function process_dwh_source {
+    ## Request processing of a DWH datasource via the POST endpoint
+    remote_source_name=$(ask_source_name "Please provide the DWH datasource name you wish to be processed")
+    if curl ${curl_args} -X POST -H "x-api-key: ${dwh_api_key}" ${DWH_API_ENDPOINT%/}/datasource/${remote_source_name}/etl ; then
+        msg_core="The DWH has accepted and queued processing of datasource '${remote_source_name}'."
+        msg_full="${msg_core} Please check the status until it changes from 'Pending' -> 'Processing' => 'Succeeded' or 'Failed'."
+        log_info ${msg_core} 
+        echo "${msg_full}"
+        return 0
+    else
+        fail_msg_core="Something went wrong requesting the datasource '${remote_source_name}' to be processed."
+        fail_msg="${fail_msg_core} Please check your connectivity and contact the DWH maintainer if the problem persists."
+        log_warn ${fail_msg_core} 
+        echo "${fail_msg}"
+        return 1
+    fi
+}
+
+function delete_dwh_source {
+    ## Request deletion of a DWH datasource
+    remote_source_name=$(ask_source_name "Please provide the DWH datasource name you wish to be deleted")
+    if curl ${curl_args} -X DELETE -H "x-api-key: ${dwh_api_key}" ${DWH_API_ENDPOINT%/}/datasource/${remote_source_name} ; then
+        msg_core="The DWH has accepted and queued deletion of datasource '${remote_source_name}'."
+        msg_full="${msg_core} Please check the status until it changes from 'Pending' -> 'Deleted'."
+        log_info ${msg_core} 
+        echo "${msg_full}"
+        return 0
+    else
+        fail_msg_core="Something went wrong requesting the datasource '${remote_source_name}' to be deleted."
+        fail_msg="${fail_msg_core} Please check your connectivity and contact the DWH maintainer if the problem persists."
+        log_warn ${fail_msg_core} 
+        echo "${fail_msg}"
+        return 1
+    fi
+}
+
+## Start logic
+scan_local_sources
+initial_options
+exit 0
