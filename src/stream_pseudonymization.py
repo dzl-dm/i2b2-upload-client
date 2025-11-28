5dd36023aad470f40f0caf5d23739a2fb6759097
Marc Griffiths <marc.griffiths@innere.med.uni-giessen.de>
2025-11-28 12:05:42 +0100

    Refactor pseudonymization to be included as a module as well as called as a script

--- src/stream_pseudonymization.py
+++ src/stream_pseudonymization.py
@@ -7,6 +7,7 @@ stderr: for logs
 
 Usage: cat ./tmp/sample-fhir.xml | src/stream-pseudonymization.py > tmp/sample-fhir-pseudonyms.xml 2>> log/pseudonymisation.log
 Explainer: Each entry is loaded into memory in turn, if its a patient, its pseudonymised, then its written
+As of 27.11.25, this can be used as a module. It can accept in/out file as opposed to only stdin/out in script mode
 """
 
 ## Import built-ins
@@ -28,7 +29,7 @@ class Settings(BaseSettings):
     """ The variables defined here will be taken from env vars if available and matching the type hint """
     log_level: str = "WARNING"
     log_format: str = "[%(asctime)s] {%(name)s/%(module)s:%(lineno)d (%(funcName)s)} %(levelname)s - %(message)s"
-    secret_key: str
+    secret_key: None|str = None
     user_mapping_filename: str = "psn-cache.tsv"
     user_mapping_separator: str = "\t"
 settings = Settings()
@@ -47,7 +48,7 @@ def is_stdin_piped():
 class FhirStream(xml.sax.ContentHandler):
     """ SAX ContentHandler to help build each <Entry> by informing an lxml class """
     entryResourceTypes:list[str] = ["Patient", "Encounter"]
-    def __init__(self, mapping_output: None|csv.DictWriter = None):
+    def __init__(self, target, mapping_output: None|csv.DictWriter = None):
         """ Ensure the XML definition is written
         Also setup the monitoring dict to detect clashes
         """
@@ -57,18 +58,16 @@ class FhirStream(xml.sax.ContentHandler):
         self.currentPatient:int = None
         self.currentEncounter:int = None
         self.mapping_output = mapping_output
+        self.target = target
 
-        ## Write the xml declaration (to stdout)
-        print('<?xml version="1.0" ?>')
+        ## Share the xml declaration (to target)
+        self.target.send(('init', ('xml', {'version': '1.0'}))) ## '<?xml version="1.0" ?>'
 
     def startElement(self, name, attrs):
         """ Depending on the element, build up the Entry sub-element in lxml """
         self.currentDepth += 1
         if name == "Bundle":
-            ## Reconstruct the Bundle opening tag
-            attrText = ', '.join([f'{k}="{v}"' for k,v in attrs.items()])
-            print(f'<Bundle {attrText}>')
-            pass
+            self.target.send(('start', ('Bundle', attrs)))
         else:
             if name in self.entryResourceTypes:
                 ## Update resource type
@@ -82,40 +81,21 @@ class FhirStream(xml.sax.ContentHandler):
             attrPairsNS:dict = {}
             for (key, value) in attrs.items(): 
                 attrPairsNS[(None, key)] = value
-            #logger.debug('sending startElement "%s" with attrs "%s" to lxml', name, attrPairsNS)
             self.currentSubElement.startElementNS((None, name), name, attrPairsNS)
-            #logger.debug('self.currentEntryResourceType: %s', self.currentEntryResourceType)
-            #logger.debug('self.currentDepth: %s', self.currentDepth)
-            #logger.debug('self.currentPatient: %s', self.currentPatient)
-            #logger.debug('self.currentEncounter: %s', self.currentEncounter)
-            #logger.debug('<%s %s>',name, attrPairsNS)
 
     def characters(self, cdata):
         """ Future proof in case we have cData in our bundle in the future """
         txt_str = cdata.strip()
         if len(txt_str) > 0:
-            #logger.debug('sending cData to lxml')
             self.currentSubElement.characters(cdata)
-        #logger.debug('self.currentEntryResourceType: %s', self.currentEntryResourceType)
-        #logger.debug('self.currentPatient: %s', self.currentPatient)
-        #logger.debug('self.currentEncounter: %s', self.currentEncounter)
-        #logger.debug('cData: %s', cdata)
 
     def endElement(self, name):
         """ If ending an entry, its time to evaluate if we pseudonymize before writing to stdout """
         self.currentDepth -= 1
         if name == "Bundle":
-            ## Write the Bundle closing tag (to stdout)
-            print('</Bundle>')
+            self.target.send(('end', 'Bundle'))
         else:
-            #logger.debug('sending endElement to lxml')
             self.currentSubElement.endElementNS((None, name), name)
-            ## Debug output...
-            #logger.debug('self.currentEntryResourceType: %s', self.currentEntryResourceType)
-            #logger.debug('self.currentPatient: %s', self.currentPatient)
-            #logger.debug('self.currentEncounter: %s', self.currentEncounter)
-            #logger.debug('</%s>', name)
-            #logger.debug('self.currentDepth: %s', self.currentDepth)
             if self.currentDepth == 1:
                 if self.currentEntryResourceType == "Patient":
                     #logger.debug("Pseudonymising patient...")
@@ -130,12 +110,9 @@ class FhirStream(xml.sax.ContentHandler):
 
     def _writeCurrentElement(self):
         """ Write the sub-element """
-        # treeXml = lxml.etree.tostring(self.currentSubElement.etree.getroot(), pretty_print=True).decode('UTF-8')
         treeXml = lxml.etree.tostring(self.currentSubElement.etree.getroot(), pretty_print=False).decode('UTF-8')
-        print(treeXml)
-        # self.fh.write(treeXml)
-        # tree.write(sys.stdout, pretty_print=True)
-        # treeXml.write(sys.stdout, pretty_print=True)
+        ## Slightly hacky, we've already constructed a sub-element, so just write it
+        self.target.send(('data', treeXml))
 
     def _cleanEncounterId(self):
         """ Use fhir id as identifier value """
@@ -166,6 +143,7 @@ class FhirStream(xml.sax.ContentHandler):
             self.currentSubElement.etree.xpath("//resource/Patient")[0].remove(nameElem)
 
     def _validAttrib(self, xpathAttrib) -> str:
+        """ Clean up the attribute in case its got multiple or zero elements."""
         if len(xpathAttrib) == 1:
             return xpathAttrib[0]
         elif len(xpathAttrib) > 1:
@@ -174,7 +152,7 @@ class FhirStream(xml.sax.ContentHandler):
         else:
             return ""
 
-def _hash_ids(given_name:str, surname:str, birthdate:str, salt:str = settings.secret_key, sep:str = "|") -> str:
+def _hash_ids(given_name:str, surname:str, birthdate:str, salt:None|str = None, sep:str = "|") -> str:
     """ Hash the combination of:
         * salt
         * given-name
@@ -182,6 +160,8 @@ def _hash_ids(given_name:str, surname:str, birthdate:str, salt:str = settings.se
         * birthdate
         with the local secret-key as the salt
     """
+    if not salt:
+        salt = settings.secret_key
     #logger.debug("Creating user pseudonym...")
     return hashlib.sha3_256(
         "{salt}{sep}{given_name}{sep}{surname}{sep}{birthdate}".format(
@@ -193,6 +173,69 @@ def _hash_ids(given_name:str, surname:str, birthdate:str, salt:str = settings.se
             ).encode('UTF-8')
         ).hexdigest()
 
+def _print_xml_target():
+    """ Prints (so stdout) each element/chunk of xml as its received. """
+    while True:
+        action = yield
+        if action is not None:
+            print(_xml_snippet_builder(action))
+
+def _write_xml_target(out_file:str):
+    """ Writes each element/chunk of xml as its received to a specified file. """
+    with open(out_file, 'w', newline='\n') as xml_out:
+        while True:
+            action = yield
+            if action is not None:
+                xml_out.write(_xml_snippet_builder(action)+"\n")
+
+
+def _xml_snippet_builder(action: tuple) -> str:
+    """ Convert tuple into XML string. """
+    xml_snippet = ""
+    # logger.debug("Processing xml action: %s", action)
+    if action[0] == 'init':
+        attr_text = ', '.join([f'{k}="{v}"' for k,v in action[1][1].items()])
+        xml_snippet = f'<?{action[1][0]} {attr_text} ?>'
+    elif action[0] == 'start':
+        attr_text = ', '.join([f'{k}="{v}"' for k,v in action[1][1].items()])
+        xml_snippet = f'<{action[1][0]} {attr_text}>'
+    elif action[0] == 'end':
+        xml_snippet = f'</{action[1]}>'
+    elif action[0] == 'data':
+        xml_snippet = action[1]
+
+    return xml_snippet
+
+def process_fhir_bundle(in_file:str, out_file:str, salt:None|str = None):
+    """ If not calling as script, use this function. """
+    logger.info("Starting fhir pseudonymization...")
+    ## TODO: This is hacky
+    if salt:
+        settings.secret_key = salt
+    else:
+        if not settings.secret_key:
+            logger.error("No secret key set! Cannot continue.")
+            return False
+    target = _write_xml_target(out_file)
+    next(target)  # Prime the generator
+
+    with open(in_file, 'r', newline='\n') as in_f:
+        mapping_headings = ["given-names","surname","birthdate","pseudonym"]
+        with open(settings.user_mapping_filename, 'w', newline='\n') as map_f:
+            mapping_writer = csv.DictWriter(map_f, delimiter=settings.user_mapping_separator, quotechar='"', quoting=csv.QUOTE_MINIMAL, fieldnames=mapping_headings, lineterminator='\n')
+            mapping_writer.writeheader()
+
+            ## Set up the sax parser
+            sp = xml.sax.make_parser()
+            sp.setContentHandler(FhirStream(target = target, mapping_output = mapping_writer))
+            sp.setFeature(xml.sax.handler.feature_namespaces, 0)
+
+            ## sax parses by emitting events when a tag or data is found, so the response to the events is all handled in the ContentHandler class above
+            sp.parse(in_f)
+
+    logger.info("Module call complete")
+    return True
+
 ## When called as script (not run if imported as module):
 if __name__ == "__main__":
     ## Application logic starts here; processing steps abstracting complexity into functions
@@ -202,6 +245,9 @@ if __name__ == "__main__":
         logger.error("This script demands piped input data from an xml fhir bundle, eg cat fhir.xml | this-script.py")
         sys.exit(1)
 
+    target = _print_xml_target()
+    next(target)  # Prime the generator
+
     mapping_headings = ["given-names","surname","birthdate","pseudonym"]
     with open(settings.user_mapping_filename, 'w', newline='\n') as f:
         mapping_writer = csv.DictWriter(f, delimiter=settings.user_mapping_separator, quotechar='"', quoting=csv.QUOTE_MINIMAL, fieldnames=mapping_headings, lineterminator='\n')
@@ -209,10 +255,11 @@ if __name__ == "__main__":
 
         ## Set up the sax parser
         sp = xml.sax.make_parser()
-        sp.setContentHandler(FhirStream(mapping_output = mapping_writer))
+        sp.setContentHandler(FhirStream(target = target, mapping_output = mapping_writer))
         sp.setFeature(xml.sax.handler.feature_namespaces, 0)
 
         ## sax parses by emitting events when a tag or data is found, so the response to the events is all handled in the ContentHandler class above
+        ## (In particulare, we use the target to control how we use the output)
         sp.parse(sys.stdin)
 
     logger.info("Script run completed!")
